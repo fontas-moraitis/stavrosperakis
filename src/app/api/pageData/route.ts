8e6b19e0ffdfc7cc17bf08239ed7bf0297a63bde
Fontas <fontas.moraitis@payaut.com>
2023-07-20 07:29:30 +0200

    testing

--- src/app/api/pageData/route.ts
+++ src/app/api/pageData/route.ts
@@ -2,15 +2,12 @@ import { NextResponse, NextFetchEvent, NextRequest } from 'next/server';
 
 export const runtime = 'edge';
 
-const ALLOWED_ORIGINS = ['http://localhost:3000/api/pageData', 'https://www.stavrosperakis.com/api/pageData'];
 const fetchUrl = `https://gapi.storyblok.com/v1/api`;
 
 export async function POST(
   request: NextRequest,
   context: NextFetchEvent,
 ) {
-  // if (!ALLOWED_ORIGINS.includes(request.url)) return NextResponse.error();
-
   const fetchOptions = {
     method: "POST",
     headers: {
@@ -22,16 +19,20 @@ export async function POST(
   };
 
   try {
-    const { data } = await fetch(fetchUrl, fetchOptions).then((response) =>
-      response.json(),
-    );
-    return NextResponse.json({
-      status: 200,
-      incomingUrl: request.url,
-      data
-    });
+    const response = await fetch(fetchUrl, fetchOptions);
+
+    if (!response.ok) return NextResponse.json({status: 500, message: response.json() });
+
+    const { data } = await response.json();
+
+    if (data) {
+      return NextResponse.json({
+        status: 200,
+        incomingUrl: request.url,
+        data
+      });
+    }
   } catch (error) {
-    // throw new Error("Could not fetch data from Storyblok!");
     return NextResponse.error();
   }
 }
