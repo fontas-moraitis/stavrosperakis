0dbb56a1bb97ec958be721465a2025a198758477
Xenofon Moraitis <xenmoraitis@gmai.com>
2023-08-05 12:37:23 +0200

    fix: cors edge fun

--- src/utils/getPageData.ts
+++ src/utils/getPageData.ts
@@ -1,9 +1,4 @@
 const getPageData = async (query: any) => {
-  let baseUrl = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' || process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' ?
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000';
-
-  const fetchUrl = `${baseUrl}/api/pageData`;
-
   const fetchOptions = {
     method: "POST",
     headers: {
@@ -14,7 +9,7 @@ const getPageData = async (query: any) => {
   };
 
   try {
-    const data = await fetch(fetchUrl, fetchOptions).then((response) =>
+    const data = await fetch('/api/pageData', fetchOptions).then((response) =>
       response.json(),
     );
 
